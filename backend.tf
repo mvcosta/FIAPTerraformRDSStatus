631e52ccb43016f1e4a7d7f55b25ca34064363db
Marcus Vinicius <mvcoosta@gmail.com>
2024-01-22 11:53:58 -0400

    altera configurações

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-backend-mv"
-    key    = "db"
+    key    = "db-status"
     region = "us-east-1"
   }
 }
--- rds.tf
+++ rds.tf
@@ -8,14 +8,14 @@ locals {
     )
 }
 
-resource "aws_db_instance" "fiapdb" {
+resource "aws_db_instance" "fiap-db-status" {
   allocated_storage      = 20 # Tamanho em GB
   storage_type           = "gp2"
   engine                 = "postgres"
   engine_version         = "12.16"
   instance_class         = "db.t2.micro"     # Tipo de instância
-  identifier             = local.db_credentials.dbInstanceIdentifier       # Identificador da instância RDS
-  name                   = "fiap"            # Nome da base de dados dentro da instância
+  identifier             = "fiap-db-status"      # Identificador da instância RDS
+  name                   = "fiapStatus"            # Nome da base de dados dentro da instância
   username               = local.db_credentials.username        # Usuário do banco de dados
   password               = local.db_credentials.password # Senha do banco de dados
   parameter_group_name   = "default.postgres12"
@@ -34,7 +34,7 @@ data "terraform_remote_state" "app" {
 }
 
 resource "aws_db_subnet_group" "db_subnet" {
-  name = "dbsubnet"
+  name = "dbstatussubnet"
   subnet_ids = [
     "${data.terraform_remote_state.app.outputs.private_subnet_a}",
     "${data.terraform_remote_state.app.outputs.private_subnet_b}",
